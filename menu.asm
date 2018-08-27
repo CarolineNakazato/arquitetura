e13ea8e73bd5393a2eac5dd4ce05b6483a6904bc
CarolineNakazato <carolinenakazato@yahoo.com.br>
2018-08-27 19:07:47 -0300

    menu pronto (excluir textos dps)
    excluir textos que são printados toda vez que entra em um comando, apenas para demonstrar e para testar!
    opcao:
    1 - cadastrar abastecimento
    2 - excluir abastecimento
    3 - exibir abastecimento
    4 - exibir consumo medio
    5 - exibir preco medio
    6 -  sair do programa

---
+++ menu.asm
@@ -0,0 +1,76 @@
+.data 
+	TEXTO: .asciiz "\nEscolha uma opcao:\n"
+	TEXTO_INVALIDO: .asciiz "\nOpcao Invalida!\n"
+	##### textos usados como auxiliar de teste #########
+	TEXTO1: .asciiz "\nTexto 1\n"
+	TEXTO2: .asciiz "\nTexto 2\n"
+	TEXTO3: .asciiz "\nTexto 3\n"
+	TEXTO4: .asciiz "\nTexto 4\n"	
+	TEXTO5: .asciiz "\nTexto 5\n"
+	####################################################
+	
+.text
+main: 
+	jal get_option # Call procedure
+	move $s0, $v0 # Put returned value in "save" reg
+
+get_option:
+	li $v0, 4 # string = 4
+	la $a0, TEXTO 
+	syscall
+
+	#le um inteiro do teclado
+	li $v0, 5 
+	syscall # value in $v0
+	
+	
+	bne $v0,1,opcao_2
+	# opcao 1 - cadastrar abastecimento
+	### chamar cadastrar abastecimento ####
+	li $v0, 4
+	la $a0, TEXTO1 
+	syscall
+	#######################################
+	j volta
+opcao_2: # 2 - excluir abastecimento
+	bne $v0,2,opcao_3
+	##### chamar excluir abastecimento #####
+	li $v0, 4 
+	la $a0, TEXTO2 
+	syscall
+	#######################################
+	j volta
+opcao_3: # 3 - exibir abastecimento
+	bne $v0,3,opcao_4
+	##### chamar exibir abastecimento #######
+	li $v0, 4 
+	la $a0, TEXTO3 
+	syscall
+	#######################################
+	j volta
+opcao_4: # 4 - exibir consumo medio
+	bne $v0,4,opcao_5
+	##### chamar exibir consumo medio #######
+	li $v0, 4 
+	la $a0, TEXTO4 
+	syscall
+	#######################################
+	j volta
+opcao_5: # 5 - exibir preco medio
+	bne $v0,5,opcao_6
+	##### chamar exibir preco medio #######
+	li $v0, 4 
+	la $a0, TEXTO5
+	syscall
+	#######################################
+	j volta
+opcao_6: # 6 -  sai do programa
+	bne $v0,6,opcao_invalido
+	li $v0, 10  # end program = 10
+    	syscall
+opcao_invalido:
+	li $v0, 4 
+	la $a0, TEXTO_INVALIDO
+	syscall
+volta:	
+	jr $ra
